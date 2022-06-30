02f3d3b20e93881b506b07ee7a47fa5fd39398f4
Trevor Coe <88930059+trevcoe@users.noreply.github.com>
2022-06-30 17:17:44 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {HiOutlineMail} from 'react-icons/hi';
+import {FaLinkedin} from 'react-icons/fa';
+import {BsGithub} from 'react-icons/bs';
+import {SiHeroku} from 'react-icons/si';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="about-me-text">
+        <p>Hello. I'm Trevor Coe - a software engineer that builds everything from websites to applications. I'm currently living and working in Ohio.</p>
+      </div>
+        <div className='social-icons'>
+          <HiOutlineMail className='social-icon-spacing'/>
+          <FaLinkedin className='social-icon-spacing'/>
+          <BsGithub className='social-icon-spacing'/>
+          <SiHeroku className='social-icon-spacing'/>
+        </div>
     </div>
   );
 }
