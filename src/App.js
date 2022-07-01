3f6bd9b66df271632bec3090f8766c93b5a05f00
Trevor Coe <88930059+trevcoe@users.noreply.github.com>
2022-06-30 20:59:00 -0400

    links/adjustedforhost

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import './App.css';
+import NavBar from './Components/NavBar';
 import {HiOutlineMail} from 'react-icons/hi';
 import {FaLinkedin} from 'react-icons/fa';
 import {BsGithub} from 'react-icons/bs';
-import {SiHeroku} from 'react-icons/si';
 
 function App() {
   return (
+    <>
+    <NavBar />
     <div className="App">
-      <div className="about-me-text">
-        <p>Hello. I'm Trevor Coe - a software engineer that builds everything from websites to applications. I'm currently living and working in Ohio.</p>
-      </div>
-        <div className='social-icons'>
-          <HiOutlineMail className='social-icon-spacing'/>
-          <FaLinkedin className='social-icon-spacing'/>
-          <BsGithub className='social-icon-spacing'/>
-          <SiHeroku className='social-icon-spacing'/>
+      <div className='underline top'>__________________</div>
+        <div className="about-me-text">
+          <p>Hello. I'm Trevor Coe - a software engineer that enjoys building front-end applications. I'm currently living and working in Ohio.</p>
         </div>
+          <div className='social-icons'>
+            <a href='//gmail.com'>
+              <HiOutlineMail className='social-icon-spacing'/>
+            </a>
+            <a href='//www.linkedin.com/in/trevor-coe12/'>
+              <FaLinkedin className='social-icon-spacing'/>
+            </a>
+            <a href='//github.com/trevcoe'>
+              <BsGithub className='social-icon-spacing'/>
+            </a>
+          </div>
+        <div className='underline'>__________________</div>
     </div>
+    </>
   );
 }
 
